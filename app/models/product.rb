ee1b3d1b10023edf76580c32526767f62003adf1
Pobduard <jeduardoa2003@gmail.com>
2025-07-08 13:05:05 -0400

    easier to read

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,12 +2,10 @@ class Product < ApplicationRecord
     # Esto de optional es para que de una vez verifique si si hay una categoria asociada al category id que le pasemos
     belongs_to :category, optional: false, counter_cache: true
     validates :name, presence: true
-    validates :price, presence: true
-    validates :category_id, presence: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
+    validates :category_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :description, presence: true
     validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-    validates :price, numericality: { greater_than: 0 }
-    validates :category_id, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :minimumQuantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
     has_one_attached :image
