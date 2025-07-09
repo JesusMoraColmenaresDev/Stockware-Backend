8f4de1b068b1554360e54532b140b179f32a539a
Jesus Mora <jesusmiguelmora.dev@gmail.com>
2025-07-09 19:20:26 -0400

    fixed devise bugs

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -323,6 +323,12 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = ENV["DEVISE_JWT_SECRET_KEY"]
+    jwt.dispatch_requests = [
+      ['POST', %r{^/users/sign_in$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/users/sign_out$}]
+    ]
     jwt.expiration_time = 1.day.to_i
   end
 end
