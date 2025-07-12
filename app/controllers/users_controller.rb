99db920b1ab7528fbcf763b6355e08e719b20574
Pobduard <jeduardoa2003@gmail.com>
2025-07-11 23:59:46 -0400

    Basic CRU Added, delete still left, no roles yet, no migration needed

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  # before_action :authenticate_user!            # require JWT
+  # before_action :authorize_user!, only: [ :show, :update ]
+  before_action :set_user, only: [ :show, :update ]
+
+
+  # GET /users (Get ALL)
+  def index
+    # Solo para Admin?
+    render json: User.all.select(:id, :email, :name, :created_at), status: :ok # Retornamos todos, pero solo los datos publivos de C/U
+  end
+
+  # GET /users/:id (Get By id)
+  def show
+    render json: @user.slice(:id, :email, :name, :created_at), status: :ok  # Del usuario especifico, nos taremos solo lo publico
+  end
+
+  # PATCH /users/:id
+  # body: { user: { name: "New Name", email: "new@example.com" } }
+  def update
+    if @user.update(user_params)  # Encuentra el usuario con los parametros respectivos?
+      render json: @user.slice(:id, :email, :name), status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
+
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    head :not_found
+  end
+
+  # Only let users update themselves (or only admins, if you add roles)
+  def authorize_user!
+    head :forbidden unless current_user.id == @user.id
+  end
+
+  def user_params
+    # Only allow name & email (and password if you want)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
